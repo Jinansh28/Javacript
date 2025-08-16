e859117344722c3749b5b1993da82ad8b7c3b1f8
Jinansh28 <jinansh2807@gmail.com>
2025-08-16 13:18:42 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" //Treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+console.log(3+
+    3)
+// It can be done but readability decrease
+console.log("hitesh")
+
+let name = "hitesh" //string
+let age = 18 //int
+let isLoggesIn = false //bool
+let state = null
+let a
+// Number = 2^53
+// bigint
+// String =>""
+// Boolean
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+// typeof null is object
+// typeof undefined is undefined
+console.log(typeof undefined);
