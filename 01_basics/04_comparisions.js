77eaeb0063682a193e6a0016f44a5470307af684
Jinansh28 <jinansh2807@gmail.com>
2025-08-16 13:54:00 +0530

    comparision

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,18 @@
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null >= 0);
+// >,< convert null to 0
+
+console.log(undefined>0)
+console.log(undefined>=0);
+console.log(undefined==0);
+// No change
+
+//=== 
+// It does not convert
+console.log("2"===2)
+
