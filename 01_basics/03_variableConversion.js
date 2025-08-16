07b4f7dd892881bd3242d6a1136fecce3f6fe04a
Jinansh28 <jinansh2807@gmail.com>
2025-08-16 13:47:08 +0530

    Operations

--- 01_basics/03_variableConversion.js
+++ 01_basics/03_variableConversion.js
@@ -24,3 +24,42 @@ let stringn = String(n)
 console.log(typeof(stringn));
 console.log(stringn);
 
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+3);
+console.log(2-3);
+console.log(3/2);
+console.log(3*2);
+console.log(2**3);
+console.log(3%2);
+
+let str1 = "hello"
+let str2 = " J"
+console.log(str1+str2);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+// Left to right 
+
+console.log(+true);
+// operation => int
+
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+console.log(gameCounter);
+
+
+
+
+
+
+
