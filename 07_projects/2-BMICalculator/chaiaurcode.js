c605d5f1f364c6459b11c90c6def1405b0c99b2e
Jinansh28 <jinansh2807@gmail.com>
2025-08-23 12:11:55 +0530

    Update Project2

--- 07_projects/2-BMICalculator/chaiaurcode.js
+++ 07_projects/2-BMICalculator/chaiaurcode.js
@@ -6,18 +6,18 @@ form.addEventListener('submit',function(e){
     const height = parseFloat(document.querySelector('#height').value)
     const weight = parseFloat(document.querySelector('#weight').value)
     const result = document.querySelector('#results')
-    if(isNaN(height) || isNaN(weight)){
+    if(isNaN(height) || isNaN(weight) || height<=0 || weight<=0){
         result.innerHTML="Not a valid value"
     }    
     
     else{
-    const BMI=(weight*100*100)/(height*height)
+    const BMI=((weight*100*100)/(height*height)).toFixed(2)
     if (BMI < 18.6) {
-    result.innerHTML = " \nUnderWeight";
+    result.innerHTML = BMI+"<br>UnderWeight";
     } else if (BMI >= 18.6 && BMI <= 24.9) {
-    result.innerHTML = " \nNormal";
+    result.innerHTML = BMI+"<br>Normal";
     } else if (BMI > 24.9) {
-    result.innerHTML = " \nOverWeight";
+    result.innerHTML = BMI+"<br>OverWeight";
     }
 }
     
